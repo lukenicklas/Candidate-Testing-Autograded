246a151ec98505bc36570f45b61bce42a2b628d1
lukenicklas <lukenicklas18@gmail.com>
2023-11-08 13:39:06 -0600

    Assignment one done

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let candidateAnswer = ""
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
-let candidateAnswers = []
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -34,10 +34,12 @@ for (let i = 0; i < questions.length; i++) {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let numberCorrectAnswers = []
 
   for (let i = 0; i < candidateAnswers.length; i++) {
-  if (candidateAnswers[i] == correctAnswers[i]){
-  console.log(`${candidateAnswers[i]} is correct! You're a genius.`);
+  if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()){
+  console.log(`${candidateAnswers[i]} is correct! You're a genius.`)
+  numberCorrectAnswers++;
 } else {
    console.log(`${candidateAnswers[i]} is incorrect. The correct answer is ${correctAnswers[i]}`);
 }
@@ -46,8 +48,24 @@ function gradeQuiz(candidateAnswers) {
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  
+  let numberQuizQuestions = questions.length
 
+  grade = ((numberCorrectAnswers)/(numberQuizQuestions))*100;
 
+  console.log(`Candidate Name: ${candidateName}`)
+  for (let i=0; i<questions.length; i++) {
+
+  console.log(questions[i]);
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+  }
+  console.log(`>>> Overall Grade: >>> ${grade}% (${numberCorrectAnswers} of ${numberQuizQuestions} responses correct) <<<`);
+  if (grade >= 80) 
+  console.log(">>> Status: PASS <<<");
+  else (
+  console.log(">>> Status: FAILED <<<"));
+  
   return grade;
 }
 
@@ -55,8 +73,10 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log(`Hello ${candidateName}!`);
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
